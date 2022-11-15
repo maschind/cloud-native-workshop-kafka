50b3751823c025ad639112c115dcbf55c7becfc6
user1 <user1@no-reply.com>
2022-11-15 15:23:33 +0000

    init serverless lab

--- cart-service/src/main/java/com/redhat/cloudnative/CartResource.java
+++ cart-service/src/main/java/com/redhat/cloudnative/CartResource.java
@@ -13,10 +13,10 @@ import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 
-// import org.apache.kafka.clients.producer.KafkaProducer;
-// import org.apache.kafka.clients.producer.Producer;
-// import org.apache.kafka.clients.producer.ProducerRecord;
-// import org.apache.kafka.common.header.internals.RecordHeaders;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.internals.RecordHeaders;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
@@ -30,12 +30,28 @@ public class CartResource {
 
     private static final Logger log = LoggerFactory.getLogger(CartResource.class);
 
-    // TODO: Add annotation of orders messaging configuration here
+    @ConfigProperty(name = "mp.messaging.outgoing.orders.bootstrap.servers")
+    public String bootstrapServers;
+
+    @ConfigProperty(name = "mp.messaging.outgoing.orders.topic")
+    public String ordersTopic;
+
+    @ConfigProperty(name = "mp.messaging.outgoing.orders.value.serializer")
+    public String ordersTopicValueSerializer;
+
+    @ConfigProperty(name = "mp.messaging.outgoing.orders.key.serializer")
+    public String ordersTopicKeySerializer;
+
+    private Producer<String, String> producer;
 
     @Inject
     ShoppingCartService shoppingCartService;
 
-    // TODO ADD getCart method
+    @GET
+    @Path("{cartId}")
+    public ShoppingCart getCart(String cartId) {
+        return shoppingCartService.getShoppingCart(cartId);
+    }
 
     @POST
     @Path("{cartId}/{itemId}/{quantity}")
@@ -57,20 +73,25 @@ public class CartResource {
 
     @POST
     @Path("/checkout/{cartId}")
-    public ShoppingCart checkout(String cartId, Order order) {
-        // TODO ADD for KAFKA
-        //sendOrder(order, cartId);
+    public ShoppingCart checkout(@PathParam("cartId") String cartId, Order order) {
+        sendOrder(order, cartId);
         return shoppingCartService.checkout(cartId);
     }
 
-    // TODO ADD for KAFKA
     private void sendOrder(Order order, String cartId) {
-
+        order.setTotal(shoppingCartService.getShoppingCart(cartId).getCartTotal() + "");
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(ordersTopic, null, null, null, Json.encode(order), new RecordHeaders().add("content-type", "application/json".getBytes()));
+        producer.send(producerRecord);
+        log.info("Sent message: " + Json.encode(order));
     }
 
-    // TODO ADD for KAFKA
     public void init(@Observes StartupEvent ev) {
+        Properties props = new Properties();
 
+        props.put("bootstrap.servers", bootstrapServers);
+        props.put("value.serializer", ordersTopicValueSerializer);
+        props.put("key.serializer", ordersTopicKeySerializer);
+        producer = new KafkaProducer<String, String>(props);
     }
 
 }
--- order-service/src/main/java/com/redhat/cloudnative/OrderResource.java
+++ order-service/src/main/java/com/redhat/cloudnative/OrderResource.java
@@ -11,10 +11,28 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.PathParam;
 
-// TODO: Add JAX-RS annotations here
-
+@Path("/api/orders")
 public class OrderResource {
 
-    // TODO: Add list(), add(), updateStatus() methods here
+    @GET
+    public List<Order> list() {
+        return Order.listAll();
+    }
+
+    @POST
+    public List<Order> add(Order order) {
+        order.persist();
+        return list();
+    }
+
+    @GET
+    @Path("/{orderId}/{status}")
+    public Order updateStatus(String orderId, String status) {
+        Order newOrder = Order.findByOrderId(orderId);
+        newOrder.status = status;
+        newOrder.update();
+        return newOrder;
+
+    }
 
 }
